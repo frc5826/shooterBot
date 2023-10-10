356c5b5c1c0b96c387acd18d476d1700b8c46d9e
FRC Team 5826 AvisAutomata <frc5826@gmail.com>
2023-10-10 18:23:58 -0500

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.*;
+
+public class TurretSubsystem extends SubsystemBase {
+
+    private CANSparkMax hood, launcher1, launcher2;
+    private TalonSRX base;
+    private DutyCycleEncoder hoodEncoder;
+    private DutyCycleEncoder baseEncoder;
+
+    private double baseZero;
+    private double hoodZero;
+
+    private PIDController basePID;
+    private PIDController hoodPID;
+
+    public TurretSubsystem() {
+        hood = new CANSparkMax(hoodID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        base = new TalonSRX(turretBaseID);
+
+        launcher1 = new CANSparkMax(turretLauncherID1, CANSparkMaxLowLevel.MotorType.kBrushless);
+        launcher2 = new CANSparkMax(turretLauncherID2, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+        hoodEncoder = new DutyCycleEncoder(hoodEncoderID);
+        baseEncoder = new DutyCycleEncoder(baseEncoderID);
+
+        hoodPID = new PIDController(1, 0, 0);
+        basePID = new PIDController(1, 0, 0);
+
+        baseZero = baseEncoder.getAbsolutePosition();
+        hoodZero = hoodEncoder.getAbsolutePosition();
+    }
+
+    @Override
+    public void periodic() {
+        //System.out.println(getYaw());
+
+    }
+
+    public double setYaw(double angle) { return basePID.calculate(angle / 360); }
+
+    public double getYaw() { return baseEncoder.getAbsolutePosition() - baseZero; }
+
+    public double setPitch(double angle) { return hoodPID.calculate(angle / 360); }
+
+    public double getPitch() { return hoodEncoder.getAbsolutePosition() - hoodZero; }
+
+    public void setLauncherRPM(int rpm) { 
+
+    }
+
+}
